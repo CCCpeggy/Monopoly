7099a0ef74dc4ab881eaa4f009cbbe702ca3ea14
rosa <u10050070@gmail.com>
2019-05-17 22:01:08 +0800

    更新上頭玩家狀態

--- Monopoly/Monopoly/Source.cpp
+++ Monopoly/Monopoly/Source.cpp
@@ -1,3 +1,10 @@
-int main() {
+#include"Grid.h"
 
+using namespace std;
+
+int main() {
+	Grid Gridline;
+	cout << Gridline.rowLine[0] << endl;
+	Gridline.showplayerlist();
+	Gridline.showcurrentplayer();
 }
